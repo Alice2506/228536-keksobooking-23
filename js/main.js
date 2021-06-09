b22fa034d45ba654620c733ddc12c889958064c5
Alice2506 <ralko84@mail.ru>
2021-06-10 00:09:20 +0300

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Вспомогательные функции
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
